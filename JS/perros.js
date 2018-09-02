64f55d61d2e4c57dd66dc71aaf329b4c93cb17b3
jamdc9 <jamdc9@gmail.com>
2018-09-02 12:19:49 -0600

    Javascript

---
+++ JS/perros.js
@@ -0,0 +1,9 @@
+/*
+git status
+git add .
+git commit -m "Comentario"
+git push origin master (me puede pedir login)
+*/
+$( "#target" ).click(function() {
+  alert( "Handler for .click() called." );
+});
