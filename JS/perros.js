718fc4e93b310a659c7b6a9bb28792568932d465
jamdc9 <jamdc9@gmail.com>
2018-09-03 13:21:27 -0600

    Todos los botones cambian sus divs

--- JS/perros.js
+++ JS/perros.js
@@ -11,11 +11,21 @@ $(document).ready(function(){
     /*Cambiamos los Divs*/
     $("#divBlacky").attr('class', 'invisible text-center');
     $("#divBrownie").attr('class', 'visible text-center');
+    $("#divJumper").attr('class', 'invisible text-center');
+    $("#divFamilia").attr('class', 'invisible text-center');
+    $("#divAmigos").attr('class', 'invisible text-center');
+
     $("#divBlacky").css('display', 'none');
     $("#divBrownie").css('display', 'block');
+    $("#divJumper").css('display', 'none');
+    $("#divFamilia").css('display', 'none');
+    $("#divAmigos").css('display', 'none');
     /*Cambiamos los botones*/
     $("#btBrownie").attr('class', 'btn btn-primary navbar-btn');
     $("#btBlacky").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btJumper").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btFamilia").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btAmigos").attr('class', 'btn btn-secondary navbar-btn');
   });
   
   $("#btBlacky").click(function() {
@@ -23,11 +33,83 @@ $(document).ready(function(){
     /*Cambiamos los Divs*/
     $("#divBlacky").attr('class', 'visible text-center');
     $("#divBrownie").attr('class', 'invisible text-center');
+    $("#divJumper").attr('class', 'invisible text-center');
+    $("#divFamilia").attr('class', 'invisible text-center');
+    $("#divAmigos").attr('class', 'invisible text-center');
+
     $("#divBlacky").css('display', 'block');
     $("#divBrownie").css('display', 'none');
+    $("#divJumper").css('display', 'none');
+    $("#divFamilia").css('display', 'none');
+    $("#divAmigos").css('display', 'none');
     /*Cambiamos los botones*/ 
     $("#btBlacky").attr('class', 'btn btn-primary navbar-btn');
     $("#btBrownie").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btJumper").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btFamilia").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btAmigos").attr('class', 'btn btn-secondary navbar-btn');
   });
+  $("#btJumper").click(function() {
+    console.log('Entro al click');
+    /*Cambiamos los Divs*/
+    $("#divBlacky").attr('class', 'invisible text-center');
+    $("#divBrownie").attr('class', 'invisible text-center');
+    $("#divJumper").attr('class', 'visible text-center');
+    $("#divFamilia").attr('class', 'invisible text-center');
+    $("#divAmigos").attr('class', 'invisible text-center');
 
+    $("#divBlacky").css('display', 'none');
+    $("#divBrownie").css('display', 'none');
+    $("#divJumper").css('display', 'block');
+    $("#divFamilia").css('display', 'none');
+    $("#divAmigos").css('display', 'none');
+    /*Cambiamos los botones*/ 
+    $("#btBlacky").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btBrownie").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btJumper").attr('class', 'btn btn-primary navbar-btn');
+    $("#btFamilia").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btAmigos").attr('class', 'btn btn-secondary navbar-btn');
+  });
+  $("#btFamilia").click(function() {
+    console.log('Entro al click');
+    /*Cambiamos los Divs*/
+    $("#divBlacky").attr('class', 'invisible text-center');
+    $("#divBrownie").attr('class', 'invisible text-center');
+    $("#divJumper").attr('class', 'invisible text-center');
+    $("#divFamilia").attr('class', 'visible text-center');
+    $("#divAmigos").attr('class', 'invisible text-center');
+
+    $("#divBlacky").css('display', 'none');
+    $("#divBrownie").css('display', 'none');
+    $("#divJumper").css('display', 'none');
+    $("#divFamilia").css('display', 'block');
+    $("#divAmigos").css('display', 'none');
+    /*Cambiamos los botones*/ 
+    $("#btBlacky").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btBrownie").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btJumper").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btFamilia").attr('class', 'btn btn-primary navbar-btn');
+    $("#btAmigos").attr('class', 'btn btn-secondary navbar-btn');
+  });
+  $("#btAmigos").click(function() {
+    console.log('Entro al click');
+    /*Cambiamos los Divs*/
+    $("#divBlacky").attr('class', 'invisible text-center');
+    $("#divBrownie").attr('class', 'invisible text-center');
+    $("#divJumper").attr('class', 'invisible text-center');
+    $("#divFamilia").attr('class', 'invisible text-center');
+    $("#divAmigos").attr('class', 'visible text-center');
+
+    $("#divBlacky").css('display', 'none');
+    $("#divBrownie").css('display', 'none');
+    $("#divJumper").css('display', 'none');
+    $("#divFamilia").css('display', 'none');
+    $("#divAmigos").css('display', 'block');
+    /*Cambiamos los botones*/ 
+    $("#btBlacky").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btBrownie").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btJumper").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btFamilia").attr('class', 'btn btn-secondary navbar-btn');
+    $("#btAmigos").attr('class', 'btn btn-primary navbar-btn');
+  });
 });
