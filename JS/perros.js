ea01a509c7db740f52e59f061d9d2f6f0b4559f0
jamdc9 <jamdc9@gmail.com>
2018-09-02 23:00:52 -0600

    StyleSheet

--- JS/perros.js
+++ JS/perros.js
@@ -4,6 +4,14 @@ git add .
 git commit -m "Comentario"
 git push origin master (me puede pedir login)
 */
-$( "#target" ).click(function() {
-  alert( "Handler for .click() called." );
+$(document).ready(function(){
+
+  $("#liBrownie").click(function() {
+    console.log('Entro al click');
+    $("#divBlacky").attr('class', 'invisible text-center');
+    $("#divBrownie").attr('class', 'visible text-center');
+    $("#divBlacky").css('display', 'none');
+    $("#divBrownie").css('display', 'block');
+  });
+
 });
