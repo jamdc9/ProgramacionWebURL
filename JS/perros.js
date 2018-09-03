7402513953d53ff8a60beef760b4c0d474594119
jamdc9 <jamdc9@gmail.com>
2018-09-02 23:33:10 -0600

    Accion en boton

--- JS/perros.js
+++ JS/perros.js
@@ -6,12 +6,28 @@ git push origin master (me puede pedir login)
 */
 $(document).ready(function(){
 
-  $("#liBrownie").click(function() {
+  $("#btBrownie").click(function() {
     console.log('Entro al click');
+    /*Cambiamos los Divs*/
     $("#divBlacky").attr('class', 'invisible text-center');
     $("#divBrownie").attr('class', 'visible text-center');
     $("#divBlacky").css('display', 'none');
     $("#divBrownie").css('display', 'block');
+    /*Cambiamos los botones*/
+    $("#btBrownie").attr('class', 'btn btn-primary navbar-btn');
+    $("#btBlacky").attr('class', 'btn btn-secondary navbar-btn');
+  });
+  
+  $("#btBlacky").click(function() {
+    console.log('Entro al click');
+    /*Cambiamos los Divs*/
+    $("#divBlacky").attr('class', 'visible text-center');
+    $("#divBrownie").attr('class', 'invisible text-center');
+    $("#divBlacky").css('display', 'block');
+    $("#divBrownie").css('display', 'none');
+    /*Cambiamos los botones*/ 
+    $("#btBlacky").attr('class', 'btn btn-primary navbar-btn');
+    $("#btBrownie").attr('class', 'btn btn-secondary navbar-btn');
   });
 
 });
