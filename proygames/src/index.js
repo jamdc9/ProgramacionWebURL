fc28fa306d9a80b5d2a57f2bc1a17ead997afcf8
jamdc9 <jamdc9@gmail.com>
2018-09-30 18:10:46 -0600

    Test Flip Card

--- proygames/src/index.js
+++ proygames/src/index.js
@@ -16,15 +16,7 @@ class VideoGames extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            juegos: [
-                { name: "Spiderman", consolas: ["PS2", "PS3", "PS4"], comentario: ["Sin jugarlo."], avatar: "https://img.game.co.uk/ml2/5/6/2/9/562944_scr9_a.png" },
-                { name: "God of War", consolas: ["PS3", "PS4", "PSVita"], comentario: ["Muy buen juego."], avatar: "https://i.blogs.es/7548d0/god-of-war-2018-gameinformer/450_1000.jpg" },
-                { name: "Pokemon", consolas: ["Game Boy Color", "Game Boy Advance", "Nintendo Game Cube", "Nintendo Wii"], comentario: ["Buenos Juegos"], avatar: "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/gamecube_12/SI_GCN_PokemonColosseum_image1600w.jpg" },
-                { name: "Sonic", consolas: ["Sega", "Nintendo(s)"], comentario: ["Infancia pura"], avatar: "https://c.slashgear.com/wp-content/uploads/2017/08/Sonic-Mania-green-hill-1-980x620.jpg" },
-                { name: "Mario Sunshine", consolas: ["Nintendo Game Cube"], comentario: ["El mejor de Mario"], avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStaF3aV8CzgtAjdgEjIGH6fPDgr6IsaqY1lRQ033Xze7OR9mz5" },
-                { name: "Age of Empires 2", consolas: ["PC"], comentario: ["Un clasico"], avatar: "https://i.ytimg.com/vi/Si6QPKiNoDY/maxresdefault.jpg" },
-                { name: "Mario Kart", consolas: ["Todas las consolas de Nintendo despues del NES"], comentario: ["Pierde Amigos"], avatar: "https://vignette.wikia.nocookie.net/mariokart/images/7/7e/Mario-kart-double-dash-1.jpg/revision/latest?cb=20140521140749&path-prefix=es" }
-            ],
+            juegos: [],
             showAdd: false,
             showEdit: false,
             currentlyEditing: 0
@@ -35,6 +27,19 @@ class VideoGames extends React.Component {
         this.editJuego = this.editJuego.bind(this);
         this.deleteJuego = this.deleteJuego.bind(this);
     }
+    //LocalStorage
+    componentDidMount() {//load the local storage data after the component renders
+        var juegos = (typeof localStorage["juegos"] !== "undefined") ? JSON.parse(localStorage.getItem("juegos")) : [
+            { name: "Spiderman", consolas: ["PS2", "PS3", "PS4"], comentario: ["Sin jugarlo."], avatar: "https://img.game.co.uk/ml2/5/6/2/9/562944_scr9_a.png" },
+            { name: "God of War", consolas: ["PS3", "PS4", "PSVita"], comentario: ["Muy buen juego."], avatar: "https://i.blogs.es/7548d0/god-of-war-2018-gameinformer/450_1000.jpg" },
+            { name: "Pokemon", consolas: ["Game Boy Color", "Game Boy Advance", "Nintendo Game Cube", "Nintendo Wii"], comentario: ["Buenos Juegos"], avatar: "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/gamecube_12/SI_GCN_PokemonColosseum_image1600w.jpg" },
+            { name: "Sonic", consolas: ["Sega", "Nintendo(s)"], comentario: ["Infancia pura"], avatar: "https://c.slashgear.com/wp-content/uploads/2017/08/Sonic-Mania-green-hill-1-980x620.jpg" },
+            { name: "Mario Sunshine", consolas: ["Nintendo Game Cube"], comentario: ["El mejor de Mario"], avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStaF3aV8CzgtAjdgEjIGH6fPDgr6IsaqY1lRQ033Xze7OR9mz5" },
+            { name: "Age of Empires 2", consolas: ["PC"], comentario: ["Un clasico"], avatar: "https://i.ytimg.com/vi/Si6QPKiNoDY/maxresdefault.jpg" },
+            { name: "Mario Kart", consolas: ["Todas las consolas de Nintendo despues del NES"], comentario: ["Pierde Amigos"], avatar: "https://vignette.wikia.nocookie.net/mariokart/images/7/7e/Mario-kart-double-dash-1.jpg/revision/latest?cb=20140521140749&path-prefix=es" }
+        ];
+        this.setState({ juegos: juegos });
+    }
     //ADD
     showAddModal() {//show the new recipe modal
         this.setState({ showAdd: !this.state.showAdd });
@@ -42,6 +47,7 @@ class VideoGames extends React.Component {
     addJuego(juego) {//create a new recipe
         let juegos = this.state.juegos;
         juegos.push(juego);
+        localStorage.setItem('juegos', JSON.stringify(juegos));
         this.setState({ juegos: juegos });
         this.showAddModal();
     }
@@ -53,6 +59,7 @@ class VideoGames extends React.Component {
         , newAvatar, currentlyEditing) {//edit an existing recipe
         let juegos = this.state.juegos;
         juegos[currentlyEditing] = { name: newName, consolas: newConsolas, avatar: newAvatar, comentario: newComentario };
+        localStorage.setItem('juegos', JSON.stringify(juegos));
         this.setState({ juegos: juegos });
         this.showEditModal(currentlyEditing);
     }
@@ -60,35 +67,53 @@ class VideoGames extends React.Component {
     deleteJuego(index) {//delete an existing recipe
         let juegos = this.state.juegos.slice();
         juegos.splice(index, 1);
+        localStorage.setItem('juegos', JSON.stringify(juegos));
         this.setState({ juegos: juegos, currentlyEditing: 0 });
     }
     render() {
         const juegos = this.state.juegos;
         return (
             <div className="jumbotron">
-                <h1>Juegos (actualmente solo muestra)</h1>
-                <Button bsStyle="primary" onClick={this.showAddModal}>Agregar Juegos</Button>
+                <h1>Juegos (CRUD / Local Storage)</h1>
+                <div className="text-center">
+                    <Button className="btn btn-outline-primary" onClick={this.showAddModal}>Agregar Juegos</Button>
+                </div>
+                <br />
                 <AddJuego onShow={this.state.showAdd} onAdd={this.addJuego} onAddModal={this.showAddModal} />
                 <div id="juegos" className="text-center">
                     <div className="row">
                         {juegos.map((juego, index) => (
-                            <Card key={index} className="col-md-6">
-                                <CardBody>
-                                    <ImageHeader imageSrc={juego.avatar} data-toggle="tooltip" data-placement="bottom" title={juego.comentario} />
-                                    <CardTitle className="title" data-toggle="tooltip" data-placement="bottom" title={juego.comentario}>{juego.name}</CardTitle>
-                                    <CardText>
-                                        {juego.consolas.map((consola, index) => (
-                                            <ListGroupItem key={index} >{consola}</ListGroupItem>
-                                        ))}
-                                    </CardText>
-                                    <ButtonToolbar>
-                                        <Button bsStyle="warning" onClick={() => { this.showEditModal(index) }}>Edit</Button>
-                                        <Button bsStyle="danger" onClick={() => {this.deleteJuego(index)}}>Delete</Button>
+                            <div className="col-xs-12 col-sm-6 col-md-4" key={index}>
+                                <div className="image-flip" ontouchstart="this.classList.toggle('hover');">
+                                    <div className="mainflip">
+                                        <div className="frontside">
+                                            <div className="card">
+                                                <div className="card-body text-center">
+                                                    <ImageHeader imageSrc={juego.avatar} data-toggle="tooltip" data-placement="bottom" title={juego.comentario} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="backside">
+                                            <div className="card">
+                                                <div className="card-body text-center mt-4">
+                                                    <h4 className="card-title">{juego.name}</h4>
+                                                    <p className="card-text">{juego.comentario}</p>
+                                                    {juego.consolas.map((consola, index) => (
+                                                        <ListGroupItem key={index} >{consola}</ListGroupItem>
+                                                    ))}
+                                                    <ButtonToolbar>
+                                                        <Button className="btn btn-outline-warning" onClick={() => { this.showEditModal(index) }}>Edit</Button>
+                                                        <Button className="btn btn-outline-danger" onClick={() => { this.deleteJuego(index) }}>Delete</Button>
 
-                                    </ButtonToolbar>
-                                </CardBody>
-                                <EditJuego onShow={this.state.showEdit} onEdit={this.editJuego} onEditModal={() => { this.showEditModal(this.state.currentlyEditing) }} currentlyEditing={this.state.currentlyEditing} juego={juegos[this.state.currentlyEditing]} />
-                            </Card>
+                                                    </ButtonToolbar>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <EditJuego onShow={this.state.showEdit} onEdit={this.editJuego} onEditModal={() => { this.showEditModal(this.state.currentlyEditing) }} currentlyEditing={this.state.currentlyEditing} juego={juegos[this.state.currentlyEditing]} />
+                                </div>
+
+                            </div>
                         ))}
                     </div>
                 </div>
